168864156868f05d4dcfb2e682eb3ffad6ae78a3
Logeshwaran21 <logeshwaran.a@diggibyte.com>
2024-03-11 15:05:37 +0530

    added gitignore file

--- src/calculate_averge/driver.py
+++ src/calculate_averge/driver.py
@@ -1,1 +1,4 @@
+import util
 
+Intput = [1,2,3,4,5]
+print(sum_of_number(Intput))
--- src/calculate_averge/util.py
+++ src/calculate_averge/util.py
@@ -1,1 +1,13 @@
+from unittest import result
+
+
+def sum_of_number(a):
+    result = 0
+    for i in range(len(a)):
+        result = i+result
+
+a=(1,2,3,4,5,6)
+sum_of_number(a)
+
+
 
