8f94b7a6dec732d18a4139bf801b7bbe3dd0d81e
Logeshwaran21 <logeshwaran.a@diggibyte.com>
2024-03-12 17:40:02 +0530

    unit_testing for average and factorial

--- test/average_test/test_average.py
+++ test/average_test/test_average.py
@@ -1,8 +1,18 @@
 import unittest
 
+from python_practice.src.calculate_averge.util import average
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
+    def test_average_valid(self):
+        Input = average([1,2,3])
+        output= 2.0
+        self.assertEqual(Input,output,"Test_case passed")  # add assertion here
+    def test_average_valid1(self):
+        Input = average([0])
+        output= "Zero Average Detected"
+        self.assertEqual(Input,output,"Test_case passed")
+    def test_average_valid_2(self):
+        Input = average([22,24,25,67,71,81])
+        output =48.33
+        self.assertEqual(Input,output,"Test_case passed")
 if __name__ == '__main__':
     unittest.main()
--- test/test_factorial/factorial_test.py
+++ test/test_factorial/factorial_test.py
@@ -1,1 +1,20 @@
+import unittest
 
+from python_practice.src.factorial.util import factorial
+
+class MyTestCase(unittest.TestCase):
+    def test_average_valid_1(self):
+        Input = factorial(5)
+        output= 120
+        self.assertEqual(Input,output)
+    def test_average_valid_2(self):
+        Input = factorial(10)
+        output= 3628800
+        self.assertEqual(Input,output)
+    def test_average_valid_3(self):
+        Input = factorial(12)
+        output= 479001600
+        self.assertEqual(Input,output)
+
+if __name__ == '__main__':
+    unittest.main()
